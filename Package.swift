dd935ce4517682d31125e8cf2ec7ffc067a6a2d5
kojirou <kojirouhtc@gmail.com>
2020-05-19 20:49:10 +0800

    indent

--- Package.swift
+++ Package.swift
@@ -3,34 +3,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "Networking",
-    products: [
-        .library(
-            name: "Networking",
-            targets: ["Networking"]),
-        .library(
-            name: "AsyncHTTPClientNetworking",
-            targets: ["AsyncHTTPClientNetworking"]),
-    ],
-    dependencies: [
-         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.0")
-    ],
-    targets: [
-        .target(
-            name: "Networking",
-            dependencies: [
-                .product(name: "NIOHTTP1", package: "swift-nio")
-        ]),
-        .target(
-            name: "AsyncHTTPClientNetworking",
-            dependencies: [
-                "Networking",
-                .product(name: "NIOFoundationCompat", package: "swift-nio"),
-                .product(name: "AsyncHTTPClient", package: "async-http-client")
-        ]),
-        .testTarget(
-            name: "NetworkingTests",
-            dependencies: ["Networking"]),
-    ]
+  name: "Networking",
+  products: [
+    .library(
+      name: "Networking",
+      targets: ["Networking"]),
+    .library(
+      name: "AsyncHTTPClientNetworking",
+      targets: ["AsyncHTTPClientNetworking"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.0")
+  ],
+  targets: [
+    .target(
+      name: "Networking",
+      dependencies: [
+        .product(name: "NIOFoundationCompat", package: "swift-nio"),
+        .product(name: "NIOHTTP1", package: "swift-nio")
+    ]),
+    .target(
+      name: "AsyncHTTPClientNetworking",
+      dependencies: [
+        "Networking",
+        .product(name: "AsyncHTTPClient", package: "async-http-client")
+    ]),
+    .testTarget(
+      name: "NetworkingTests",
+      dependencies: ["Networking"]),
+  ]
 )
