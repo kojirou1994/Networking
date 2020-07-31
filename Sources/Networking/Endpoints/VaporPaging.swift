5523a872886d3545d27dc0ef49a17df9b691d43b
kojirou <kojirouhtc@gmail.com>
2020-08-01 01:56:59 +0800

    provide VaporPaging endpoint

---
+++ Sources/Networking/Endpoints/VaporPaging.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+public struct VaporPage<T>: Decodable where T: Decodable {
+  public let items: [T]
+  public let metadata: VaporPageMetadata
+}
+
+public struct VaporPageMetadata: Decodable {
+  public let page: Int
+  public let per: Int
+  public let total: Int
+}
+
+public struct VaporPaging<Base>: Endpoint
+where Base: Endpoint, Base.ResponseBody: Decodable {
+
+  /// Page number to request. Starts at `1`.
+  public let page: Int
+  /// Max items per page.
+  public let itemsPerPage: Int
+  public let base: Base
+
+  public init(_ base: Base, page: Int, itemsPerPage: Int) {
+    self.page = page
+    self.itemsPerPage = itemsPerPage
+    self.base = base
+  }
+
+  public var body: Base.RequestBody { base.body }
+  public var path: String { base.path }
+  public var contentType: ContentType { base.contentType }
+  public var acceptType: ContentType { base.acceptType }
+  public var headers: HTTPHeaders { base.headers }
+  public var queryItems: [URLQueryItem] {
+    base.queryItems
+      + [.init(name: "page", value: page.description),
+         .init(name: "per", value: itemsPerPage.description)]
+  }
+
+  public typealias ResponseBody = VaporPage<Base.ResponseBody>
+
+}
