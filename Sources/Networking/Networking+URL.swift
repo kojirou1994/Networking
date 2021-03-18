c6607130edf21a1aee657875ad4554c30cf3aa2c
kojirou <kojirouhtc@gmail.com>
2021-03-19 03:43:37 +0800

    use base path in shared URLComponents

--- Sources/Networking/Networking+URL.swift
+++ Sources/Networking/Networking+URL.swift
@@ -4,7 +4,15 @@ extension Networking {
   @_transparent
   public func url<E: Endpoint>(for endpoint: E) -> URL {
     var components = urlComponents
-    components.path = endpoint.path
+    let path = endpoint.path
+    if !path.isEmpty {
+      if components.path.isEmpty {
+        assert(path.first == "/", "path must begin with '/'.")
+        components.path = endpoint.path
+      } else {
+        components.path.append(endpoint.path)
+      }
+    }
     if components.queryItems == nil {
       components.queryItems = endpoint.queryItems
     } else {
