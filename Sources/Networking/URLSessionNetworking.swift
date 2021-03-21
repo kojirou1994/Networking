79ccb402ad0fdf1c07d0e0aae5e78d5bc2b07e4c
kojirou <kojirouhtc@gmail.com>
2021-03-21 15:45:15 +0800

    add autoResume to URLSessionNetworking protocol

--- Sources/Networking/URLSessionNetworking.swift
+++ Sources/Networking/URLSessionNetworking.swift
@@ -9,11 +9,20 @@ where Request == URLRequest, Response == HTTPURLResponse,
 
   var session: URLSession { get }
 
+  /// auto call URLSessionTask.resume()
+  var autoResume: Bool { get }
+
 }
 
 extension URLSessionNetworking {
+
+  @inlinable
+  public var autoResume: Bool {
+    false
+  }
+
   public func execute(_ request: URLRequest, completion: @escaping (RawResult) -> Void) -> Task {
-    session.dataTask(with: request) { data, response, error in
+    let task = session.dataTask(with: request) { data, response, error in
       guard error == nil else {
         completion(.failure(error as! URLError))
         return
@@ -21,5 +30,9 @@ extension URLSessionNetworking {
       let res = response as! HTTPURLResponse
       completion(.success(.init(response: res, body: data ?? Data())))
     }
+    if autoResume {
+      task.resume()
+    }
+    return task
   }
 }
