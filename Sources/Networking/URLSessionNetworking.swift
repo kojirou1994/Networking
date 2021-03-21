3fdc247c8b6b8b6148fda9c7ea0e9a27dbe78e31
kojirou <kojirouhtc@gmail.com>
2021-03-21 16:35:11 +0800

    still logging

--- Sources/Networking/URLSessionNetworking.swift
+++ Sources/Networking/URLSessionNetworking.swift
@@ -25,10 +25,23 @@ extension URLSessionNetworking {
   public func execute(_ request: URLRequest, completion: @escaping (RawResult) -> Void) -> Task {
     let task = session.dataTask(with: request) { data, response, error in
       guard error == nil else {
+        #if NETWORKING_LOGGING
+        if #available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *) {
+          logger.error("URLRequest execute failed! Error: \((error as! URLError).localizedDescription)")
+        }
+        #endif
         completion(.failure(error as! URLError))
         return
       }
       let res = response as! HTTPURLResponse
+      #if NETWORKING_LOGGING
+      if #available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *) {
+        logger.debug("URLRequest execute successed!")
+        logger.debug("Response status code: \(res.statusCode)")
+        logger.debug("Response headers: \(res.allHeaderFields)")
+        logger.debug("Response body: \(data?.count ?? 0) bytes")
+      }
+      #endif
       completion(.success(.init(response: res, body: data ?? Data())))
     }
     if autoResume {
