2ad23f60b0535073ae733281e995533d82fbbeba
kojirou <kojirouhtc@gmail.com>
2022-02-11 22:07:31 +0800

    add template StatusValidatedEndpoint

---
+++ Sources/EndpointTemplate/StatusValidatedEndpoint.swift
@@ -0,0 +1,31 @@
+import Networking
+
+public struct InvalidHTTPStatus: Error {
+  @inlinable
+  internal init(status: HTTPResponseStatus, headers: HTTPHeaders) {
+    self.status = status
+    self.headers = headers
+  }
+
+  public let status: HTTPResponseStatus
+  public let headers: HTTPHeaders
+}
+
+public protocol StatusValidatedEndpoint: Endpoint {
+  var validStatusCodes: Range<HTTPResponseStatus> { get }
+}
+
+extension StatusValidatedEndpoint {
+  @inlinable
+  public var validStatusCodes: Range<HTTPResponseStatus> {
+    .ok..<HTTPResponseStatus(statusCode: 300)
+  }
+
+  @inlinable
+  public func validate<N: Networking>(networking: N, response: N.RawResponse) throws {
+    // the body is ignored
+    if !validStatusCodes.contains(response.response.status) {
+      throw InvalidHTTPStatus(status: response.response.status, headers: response.response.headers)
+    }
+  }
+}
